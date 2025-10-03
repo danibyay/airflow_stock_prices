38382fe61e626dfc259754d9faefa9cb1444e0c8
Daniela Becerra Gonzalez <danybecerr@gmail.com>
2025-10-02 21:08:55 -0600

    Create store_prices task that creates object in a minio bucket

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -4,7 +4,7 @@ from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-from include.stock_market.tasks import _get_stock_prices
+from include.stock_market.tasks import _get_stock_prices, _store_prices
 
 SYMBOL = 'NVDA'
 
@@ -34,7 +34,13 @@ def stock_market():
         op_kwargs={'url': '{{ ti.xcom_pull(task_ids="is_api_available") }}', 'symbol': SYMBOL}
     )
     
-    is_api_available() >> get_stock_prices
+    store_prices = PythonOperator(
+        task_id='store_prices',
+        python_callable=_store_prices,
+        op_kwargs={'stock': '{{ ti.xcom_pull(task_ids="get_stock_prices") }}'}
+    )
+
+    is_api_available() >> get_stock_prices >> store_prices
         
 
 stock_market()
